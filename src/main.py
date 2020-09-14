6062317d8ee1b960a5ab7d907e735a3ca816c17b
Hakon Kjaernli <hakon.slatten.kjaernli@tronderenergi.no>
2020-09-14 07:01:32 +0000

    Refactor main

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from gluonts.trainer import Trainer
 import mxnet as mx
 import numpy as np
 
-from cross_val import nested_cross_validation
 from dataloader import DataLoader
 from model_utils import create_model, evaluate_model, evaluate_n_times
 from result_utils import plot_forecast, write_results
@@ -44,67 +43,53 @@ def run_experiment(exp_path: str, config: dict, num_tests: int = 0, test_seed: i
     if config["hyperparams"]["prediction_length"] is None:
         config["hyperparams"]["prediction_length"] = dataloader.prediction_length
 
-    if config['cross_val']:
-        nested_cross_validation(
-            exp_path,
-            dataloader.test_data,
-            config["model_name"],
-            config["hyperparams"],
-            config["trainer"]
-        )
+    # Run a single experiment
+    # Define a trainer
+    if not config.get("track_training"):
+        trainer = Trainer(**config["trainer"])
     else:
-        # Run a single experiment
-        # Define a trainer
-        if not config.get("track_training"):
-            trainer = Trainer(**config["trainer"])
-        else:
-            trainer = TrackingTrainer(
-                config["model_name"],
-                config["dataset_name"],
-                config["random_seed"],
-                **config["trainer"])
-
-        # Create, train and test the model
-        estimator = create_model(
+        trainer = TrackingTrainer(
             config["model_name"],
-            trainer,
-            **config["hyperparams"]
+            config["dataset_name"],
+            config["random_seed"],
+            **config["trainer"])
+
+    # Create, train and test the model
+    estimator = create_model(
+        config["model_name"],
+        trainer,
+        **config["hyperparams"]
+    )
+    predictor = estimator.train(
+        dataloader.train_data,
+        validation_data=dataloader.val_data if config["use_val_data"] else None
+    )
+    forecasts, targets, metrics = evaluate_model(predictor, dataloader.test_data, 100)
+
+    # Save weights
+    if config.get("save_weights"):
+        predictor.serialize_prediction_net(pathlib.Path(exp_path))
+        os.remove(f"{exp_path}/prediction_net-network.json")
+    
+    # Store results
+    write_results(forecasts, targets, metrics, config["hyperparams"]["prediction_length"], f"{exp_path}/")
+    plot_forecast(targets, forecasts, f"{exp_path}/plot.png")
+
+    # Sample multiple times with the same seed if we specify it.
+    if num_tests > 0:
+        # Create a folder for aditional tests
+        if not os.path.exists(f"{exp_path}/samples"):
+            os.mkdir(f"{exp_path}/samples")
+
+        # Evaluate the model multiple times with different seeds
+        evaluate_n_times(
+            predictor,
+            dataloader,
+            config["hyperparams"]["prediction_length"],
+            num_tests,
+            test_seed,
+            exp_path
         )
-        predictor = estimator.train(
-            dataloader.train_data,
-            validation_data=dataloader.val_data if config["use_val_data"] else None
-        )
-
-        # if config["trainer"]["ctx"] == "gpu" and config["model_name"] == "deepstate":
-        #     temp_dir = pathlib.Path(f"temps/{config['dataset_name']}{config['random_seed']}")
-        #     os.mkdir(temp_dir)
-        #     predictor.serialize(temp_dir)
-        #     predictor = predictor.deserialize(temp_dir, ctx=mx.context.cpu())
-
-        forecasts, targets, metrics = evaluate_model(predictor, dataloader.test_data, 100)
-
-        # Save weights and delete unnecessary file
-        if config.get("save_weights"):
-            predictor.serialize_prediction_net(pathlib.Path(exp_path))
-            os.remove(f"{exp_path}/prediction_net-network.json")
-        
-        write_results(forecasts, targets, metrics, config["hyperparams"]["prediction_length"], f"{exp_path}/")
-        plot_forecast(targets, forecasts, f"{exp_path}/plot.png")
-
-        if num_tests > 0:
-            # Create a folder for aditional tests
-            if not os.path.exists(f"{exp_path}/samples"):
-                os.mkdir(f"{exp_path}/samples")
-
-            # Evaluate the model multiple times with different seeds
-            evaluate_n_times(
-                predictor,
-                dataloader,
-                config["hyperparams"]["prediction_length"],
-                num_tests,
-                test_seed,
-                exp_path
-            )
 
 
 if __name__ == "__main__":
